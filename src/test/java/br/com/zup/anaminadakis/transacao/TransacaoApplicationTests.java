be44137e46219e729bf6fb9c4e87200aea1c522b
Ana Carolina Minadakis <ana.minadakis@zup.com.br>
2021-07-12 14:10:15 -0300

    setup do projeto

---
+++ src/test/java/br/com/zup/anaminadakis/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.anaminadakis.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
